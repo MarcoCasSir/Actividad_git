c5a29fcf6e99476fc65795c70d17582a6e6b1e44
Sergio Parra <parra52@gmail.com>
2024-05-07 16:12:00 +0200

    Sergio ha implantado otra clase

---
+++ src/sergio/SergioParra.java
@@ -0,0 +1,14 @@
+package sergio;
+
+public class SergioParra {
+
+	public static void main(String[] args) {
+		System.out.println(" Hola me llamo Sergio");
+
+	}
+
+	
+	public void saludar() {
+        System.out.println("Hola, soy Ana");
+    }
+}
